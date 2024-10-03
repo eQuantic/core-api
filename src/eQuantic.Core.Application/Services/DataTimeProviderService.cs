f29a850f54b211c06108b944bbfb6a7995fa3e1b
Edgar Mesquita <edgar.rj@gmail.com>
2024-10-03 21:05:40 +0100

    feat: DateTime Provider Service implementation

---
+++ src/eQuantic.Core.Application/Services/DataTimeProviderService.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace eQuantic.Core.Application.Services;
+
+public class DataTimeProviderService : IDateTimeProviderService
+{
+#if NET8_0
+     private readonly TimeProvider _timeProvider;
+
+     public DataTimeProviderService(TimeProvider timeProvider)
+     {
+          _timeProvider = timeProvider;
+     }
+
+     public DateTimeOffset GetLocalNow()
+     {
+          return _timeProvider.GetLocalNow();
+     }
+
+     public DateTimeOffset GetUtcNow()
+     {
+          return _timeProvider.GetUtcNow();
+     }
+
+     public long GetTimestamp()
+     {
+          return _timeProvider.GetTimestamp();
+     }
+#else
+     public DateTimeOffset GetLocalNow()
+     {
+          return DateTimeOffset.Now;
+     }
+
+     public DateTimeOffset GetUtcNow()
+     {
+          return DateTimeOffset.UtcNow;
+     }
+
+     public long GetTimestamp()
+     {
+          return Stopwatch.GetTimestamp();
+     }
+#endif
+}
